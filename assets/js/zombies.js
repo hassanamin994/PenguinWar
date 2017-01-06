623eda95384a0eb03789622504d43aa1b53927d9
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-01-06 22:31:31 +0200

    Improve Zombies Obj

--- assets/js/zombies.js
+++ assets/js/zombies.js
@@ -1,6 +1,29 @@
 var ZOMBIES = {
     init: function (options) {
+        document.onkeydown = function(evt) {
+            ZOMBIES.toggleKey(evt.keyCode, true);
+        };
 
+        document.onkeyup = function(evt) {
+            ZOMBIES.toggleKey(evt.keyCode, false);
+        };
+    },
+    toggleKey : function (keyCode, isPressed) {
+        if (keyCode == LEFT_KEY) {
+            //controller.left = isPressed;
+        }
+        if (keyCode == RIGHT_KEY) {
+            //controller.right = isPressed;
+        }
+        if (keyCode == UP_KEY) {
+            //controller.up = isPressed;
+        }
+        if (keyCode == DOWN_KEY) {
+            //controller.down = isPressed;
+        }
+        if (keyCode == SPACE_KEY) {
+            //controller.space = isPressed;
+        }
     },
     helpers: {
         getRandom : function (maxSize) {
