c98a3bd656b620d749283a68e9f68467f8abae80
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-01-06 21:28:47 +0200

    Add Basic Js file

---
+++ assets/js/zombies.js
@@ -0,0 +1,10 @@
+var ZOMBIES = {
+    init: function (options) {
+
+    },
+    helpers: {
+        getRandom : function (maxSize) {
+            return parseInt(Math.random() * maxSize);
+        }
+    }
+};
