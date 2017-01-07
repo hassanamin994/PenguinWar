6aca61c655728ecde36933006206aaa9403fd594
kazafy <kazafysayed@gmail.com>
2017-01-07 21:30:30 +0200

    handel enemy move

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -2,22 +2,24 @@ Enemy.prototype = Object.create(Component.prototype);
 Enemy.prototype.constructor  = Enemy;
 
 function Enemy(img,height,width) {
-	Component.call(this,img, ZOMBIES.helpers.getRandom(GAME_WIDTH),-40,height,width,'enemy' + ZOMBIES.helpers.getRandom(1000000));
-	this.element = document.createElement('img');
+	Component.call(this,img, ZOMBIES.helpers.getRandom(GAME_WIDTH),40,height,width,'enemy' + ZOMBIES.helpers.getRandom(1000000),'enemy');
+//	this.element = document.createElement('img');
 	this.move =function(){
     this.y += 4;
     this.x += ZOMBIES.helpers.getRandom(7) - 3;
     this.setPosition(this.x , this.y) ;
   };
 }
+Enemy.prototype.gotOut = function () {
 
-var enemy = new Enemy('img',100,20) ;
-console.log(enemy.x,enemy.y) ;
-enemy.move();
+};
+// var enemy = new Enemy('img',100,20) ;
+// console.log(enemy.x,enemy.y) ;
+// enemy.move();
 
-console.log(enemy.x,enemy.y) ;
-enemy.move();
-console.log(enemy.x,enemy.y) ;
-enemy.move();
-console.log(enemy.x,enemy.y) ;
-enemy.move();
+// console.log(enemy.x,enemy.y) ;
+// enemy.move();
+// console.log(enemy.x,enemy.y) ;
+// enemy.move();
+// console.log(enemy.x,enemy.y) ;
+// enemy.move();
--- assets/js/zombies.js
+++ assets/js/zombies.js
@@ -3,16 +3,20 @@ var ZOMBIES = {
         timeInit: 0,
         HEROWIDTH: 80,
         HEROHEIGHT: 80,
-
+        enemies:[],
+        LEVEL : 1,
+        LEVEL_SPEED_Enemy_Generate : 500,
+        LEVEL_SPEED_Enemy_Move : 1,
+        iterations:0,
         LEFT_KEY: 65,
         UP_KEY: 87,
         RIGHT_KEY: 68,
         DOWN_KEY: 83,
         SPACE_KEY: 32,
-
         LAZER_SPEED: 25,
-
         laserArray: [],
+        ENEMYHEIGHT: 40,
+        ENEMYWIDTH: 40,
 
         init: function (options) {
 
@@ -30,17 +34,18 @@ var ZOMBIES = {
         }
         ,
         loop: function () {
+            
             if (new Date().getTime() - this.lastLoopRun > 40) {
                 this.updatePositions();
                 this.hero.handelControllers();
                 // this.checkCollisions();
 
-                // this.addEnemy();
+                this.addEnemy();
 
                 // this.showSprites();
 
                 this.lastLoopRun = new Date().getTime();
-                //iterations++;
+                  this.iterations ++;
             }
             setTimeout('ZOMBIES.loop();', 41);
         }
@@ -55,6 +60,16 @@ var ZOMBIES = {
                     ZOMBIES.laserArray[i].y -= ZOMBIES.LAZER_SPEED;
                 }
             }
+            for( var i = 0; i < this.enemies.length ; i++){ 
+                this.enemies[i].y += this.LEVEL_SPEED_Enemy_Move;
+                if (ZOMBIES.enemies[i].isGotOut) {
+                    ZOMBIES.enemies[i].remove();
+                    ZOMBIES.enemies.splice(i,1);
+                }else{
+                    ZOMBIES.enemies[i].y += ZOMBIES.LAZER_SPEED;
+                }
+            }
+
         },
         toggleKey: function (keyCode, isPressed) {
             if (keyCode == ZOMBIES.LEFT_KEY) {
@@ -81,7 +96,28 @@ var ZOMBIES = {
                 return parseInt(Math.random() * maxSize);
             }
         }
+        ,addEnemy:function () {
+  this.interval = 50;
+
+console.log('addEnemy')
+  if (this.iterations > ZOMBIES.LEVEL * ZOMBIES.LEVEL_SPEED_Enemy_Generate) {
+    this.interval = 5;
+  } else if (this.iterations > ZOMBIES.LEVEL * ZOMBIES.LEVEL_SPEED_Enemy_Generate) {
+    this.interval = 20;
+  } else if (this.iterations > ZOMBIES.LEVEL * ZOMBIES.LEVEL_SPEED_Enemy_Generate) {
+    this.interval = 35;
+  }
+  
+  if (this.helpers.getRandom(this.interval) == 0) {
+
+    var elementName = 'enemy' + this.helpers.getRandom(10000000);
+    var enemy = new Enemy('assets/images/heros/male-hero.png' ,this.ENEMYHEIGHT,this.ENEMYWIDTH ); 
+
+    console.log(enemy);   
+    this.enemies.push (enemy);
+  }
+}
+
     }
     ;
-
 ZOMBIES.init();
