35909361d42e83504ea7f8848c0dd42eebf88669
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-01-07 16:06:30 +0200

    Move keys by w/s/a/d

--- assets/js/zombies.js
+++ assets/js/zombies.js
@@ -4,10 +4,10 @@ var ZOMBIES = {
         HEROWIDTH: 80,
         HEROHEIGHT: 80,
 
-        LEFT_KEY: 37,
-        UP_KEY: 38,
-        RIGHT_KEY: 39,
-        DOWN_KEY: 40,
+        LEFT_KEY: 65,
+        UP_KEY: 87,
+        RIGHT_KEY: 68,
+        DOWN_KEY: 83,
         SPACE_KEY: 32,
 
 
